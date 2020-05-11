eeb451b0fa297b9562cb48e8b0febc8598eb155a
Ghostkeeper <rubend@tutanota.com>
2020-05-11 23:59:36 +0200

    Add Blender info
    I hope this is more or less correct. I'm taking this from the add-on tutorial (https://docs.blender.org/manual/en/latest/advanced/scripting/addon_tutorial.html) as well as from the STL add-on.

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# Blender plug-in to import and export 3MF files.
+# Copyright (C) 2020 Ghostkeeper
+# This plug-in is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+# You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
+
+bl_info = {
+	"name": "3MF format",
+	"author": "Ghostkeeper",
+	"version": (0, 1, 0),
+	"blender": (2, 82, 0),
+	"location": "File > Import-Export",
+	"description": "Import-Export 3MF files",
+	"category": "Import-Export"
+}
+
+"""
+Import and export 3MF files in Blender.
+"""
