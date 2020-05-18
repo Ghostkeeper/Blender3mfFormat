6c6137a63b97526f8e0448e2e5ff83f3dad3581b
Ghostkeeper <rubend@tutanota.com>
2020-05-18 23:16:44 +0200

    Apply correct unit to the output
    This takes the scene's units and always converts them into millimetres.

--- export_3mf.py
+++ export_3mf.py
@@ -9,6 +9,7 @@ import bpy.props  # To define metadata properties for the operator.
 import bpy.types  # This class is an operator in Blender, and to find meshes in the scene.
 import bpy_extras.io_utils  # Helper functions to export meshes more easily.
 import logging  # To debug and log progress.
+import mathutils  # For the transformation matrices.
 import xml.etree.ElementTree  # To write XML documents with the 3D model data.
 import zipfile  # To write zip archives, the shell of the 3MF file.
 
@@ -17,9 +18,11 @@ from .constants import (
 	threemf_content_types_location,
 	threemf_content_types_xml,
 	threemf_default_namespace,
+	threemf_default_unit,
 	threemf_rels_location,
 	threemf_rels_xml
 )
+from .unit_conversions import blender_to_metre, threemf_to_metre
 
 log = logging.getLogger(__name__)
 
@@ -60,11 +63,13 @@ class Export3MF(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
 		else:
 			blender_objects = context.scene.objects
 
+		global_scale = self.unit_scale(context)
+
 		# Due to an open bug in Python (as of Blender's version) we need to prefix all elements with the namespace.
 		# Bug: https://bugs.python.org/issue17088
 		# Workaround: https://stackoverflow.com/questions/4997848/emitting-namespace-specifications-with-elementtree-in-python/4999510#4999510
 		root = xml.etree.ElementTree.Element("{{{ns}}}model".format(ns=threemf_default_namespace))
-		self.write_objects(root, blender_objects)
+		self.write_objects(root, blender_objects, global_scale)
 
 		document = xml.etree.ElementTree.ElementTree(root)
 		with archive.open(threemf_3dmodel_location, "w") as f:
@@ -92,27 +97,33 @@ class Export3MF(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
 
 		return archive
 
-	def write_objects(self, root, blender_objects):
+	def write_objects(self, root, blender_objects, global_scale):
 		"""
 		Writes a group of objects into the 3MF archive.
 		:param root: An XML root element to write the objects into.
 		:param blender_objects: A list of Blender objects that need to be
 		written to that XML element.
+		:param global_scale: A scaling factor to apply to all objects to convert
+		the units.
 		"""
+		transformation = mathutils.Matrix.Scale(global_scale)
+
 		resources_element = xml.etree.ElementTree.SubElement(root, "{{{ns}}}resources".format(ns=threemf_default_namespace))
 		for blender_object in blender_objects:
 			if blender_object.parent is None:  # Only write objects that have no parent, since we'll get the child objects recursively.
 				if not isinstance(blender_object.data, bpy.types.Mesh):
 					continue
-				objectid = self.write_object_resource(resources_element, blender_object)
+				objectid = self.write_object_resource(resources_element, blender_object, transformation)
 
-	def write_object_resource(self, resources_element, blender_object):
+	def write_object_resource(self, resources_element, blender_object, transformation):
 		"""
 		Write a single Blender object and all of its children to the resources
 		of a 3MF document.
 		:param resources_element: The <resources> element of the 3MF document to
 		write into.
 		:param blender_object: A Blender object to write to that XML element.
+		:param transformation: A transformation to apply to this object before
+		writing it to the file.
 		:return: The object ID of the newly written resource.
 		"""
 		new_resource_id = self.next_resource_id
@@ -127,8 +138,30 @@ class Export3MF(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
 			for child in blender_object.children:
 				if not isinstance(child.data, bpy.types.Mesh):
 					continue
-				child_id = self.write_object_resource(resources_element, child)  # Recursively write children to the resources.
+				child_id = self.write_object_resource(resources_element, child, transformation)  # Recursively write children to the resources.
 				component_element = xml.etree.ElementTree.SubElement(components_element, "{{{ns}}}component".format(ns=threemf_default_namespace))
 				component_element.attrib["{{{ns}}}objectid".format(ns=threemf_default_namespace)] = str(child_id)
 
 		return new_resource_id
+
+	def unit_scale(self, context):
+		"""
+		Get the scaling factor we need to transform the document to millimetres.
+		:param context: The Blender context to get the unit from.
+		:return: Floating point value that we need to scale this model by. A
+		small number (<1) means that we need to make the coordinates in the 3MF
+		file smaller than the coordinates in Blender. A large number (>1) means
+		we need to make the coordinates in the file larger than the coordinates
+		in Blender.
+		"""
+		scale = self.global_scale
+
+		if context.scene.unit_settings.scale_length != 0:
+			scale *= context.scene.unit_settings.scale_length  # Apply the global scale of the units in Blender.
+
+		threemf_unit = threemf_default_unit
+		blender_unit = context.scene.unit_settings.length_unit
+		scale /= threemf_to_metre[threemf_unit]  # Convert 3MF units to metre.
+		scale *= blender_to_metre[blender_unit]  # Convert metre to Blender's units.
+
+		return scale
