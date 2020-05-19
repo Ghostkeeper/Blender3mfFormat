9d387dd3da379ff73bbcf8aa58e589824a316965
Ghostkeeper <rubend@tutanota.com>
2020-05-20 00:44:54 +0200

    Add basis for an automated test framework

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+# Blender add-on to import and export 3MF files.
+# Copyright (C) 2020 Ghostkeeper
+# This add-on is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This add-on is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+# You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
+
+# This page intentionally left blank.
