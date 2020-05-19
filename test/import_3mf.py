c164351d90433110762ca19c4fa2f5a48e4860ff
Ghostkeeper <rubend@tutanota.com>
2020-05-20 01:40:30 +0200

    Fix importing the class under test without bpy
    Setting up an environment with bpy is quite a hassle. I can make this work locally, but to create scripts that automatically set this up on a CI server is a hassle. I'd rather mock the entire API out, which is better for separation of dependencies anyway.

--- test/import_3mf.py
+++ test/import_3mf.py
@@ -4,8 +4,29 @@
 # This add-on is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
 
-import unittest
+import unittest  # To run the tests.
+import unittest.mock  # To mock away the Blender API.
+import sys  # To mock entire packages.
 
+from .mock.bpy import MockOperator, MockExportHelper, MockImportHelper
+
+# Mock all of the Blender API packages.
+sys.modules["bpy"] = unittest.mock.MagicMock()
+sys.modules["bpy.props"] = unittest.mock.MagicMock()
+sys.modules["bpy.types"] = unittest.mock.MagicMock()
+sys.modules["bpy.utils"] = unittest.mock.MagicMock()
+sys.modules["bpy_extras"] = unittest.mock.MagicMock()
+sys.modules["bpy_extras.io_utils"] = unittest.mock.MagicMock()
+
+# The import and export classes inherit from classes from the Blender API. These classes would be MagicMocks as well.
+# However their metaclasses are then also MagicMocks, but different instances of MagicMock.
+# Python sees this as that the metaclasses that ImportHelper/ExportHelper inherits from are not the same and raises an error.
+# So here we need to specify that the classes that they inherit from are NOT MagicMock but just an ordinary mock object.
+import bpy.types
+import bpy_extras.io_utils
+bpy.types.Operator = MockOperator
+bpy_extras.io_utils.ImportHelper = MockImportHelper
+bpy_extras.io_utils.ExportHelper = MockExportHelper
 import io_mesh_3mf.import_3mf
 
 class TestImport3MF(unittest.TestCase):
---
+++ test/mock/bpy.py
@@ -0,0 +1,22 @@
+# Blender add-on to import and export 3MF files.
+# Copyright (C) 2020 Ghostkeeper
+# This add-on is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This add-on is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+# You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
+
+"""
+This module contains mocks for Blender's API.
+
+The mocks in this module are meant to be very basic mocks. They will not return
+anything special or do anything. They are just meant to remove the basic import
+errors and add the missing names.
+"""
+
+class MockOperator:
+	pass
+
+class MockImportHelper:
+	pass
+
+class MockExportHelper:
+	pass
