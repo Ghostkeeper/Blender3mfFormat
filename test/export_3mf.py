aca23e80e0deb4aa4ae436683c11dae510ac7c98
Ghostkeeper <rubend@tutanota.com>
2020-05-21 18:43:02 +0200

    Add test for creating an empty archive
    It's not quite empty though.

--- test/export_3mf.py
+++ test/export_3mf.py
@@ -4,6 +4,8 @@
 # This add-on is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this plug-in. If not, see <https://gnu.org/licenses/>.
 
+import os  # To save archives to a temporary file.
+import tempfile  # To save archives to a temporary file.
 import unittest  # To run the tests.
 import unittest.mock  # To mock away the Blender API.
 import sys  # To mock entire packages.
@@ -28,6 +30,12 @@ bpy.types.Operator = MockOperator
 bpy_extras.io_utils.ImportHelper = MockImportHelper
 bpy_extras.io_utils.ExportHelper = MockExportHelper
 import io_mesh_3mf.export_3mf  # Now we may safely import the unit under test.
+from io_mesh_3mf.constants import (
+	threemf_content_types_location,
+	threemf_content_types_xml,
+	threemf_rels_location,
+	threemf_rels_xml
+)
 
 class TestExport3MF(unittest.TestCase):
 	"""
@@ -39,3 +47,22 @@ class TestExport3MF(unittest.TestCase):
 		Creates fixtures to help running these tests.
 		"""
 		self.exporter = io_mesh_3mf.export_3mf.Export3MF()  # An exporter class.
+
+	def test_create_archive(self):
+		"""
+		Tests creating an empty archive.
+
+		While the archive may be void of 3D data, it still has some metadata
+		files in it. This tests if those files are created correctly.
+		"""
+		try:
+			file_handle, file_path = tempfile.mkstemp()
+			os.close(file_handle)
+			archive = self.exporter.create_archive(file_path)
+
+			self.assertSetEqual(set(archive.namelist()), {threemf_rels_location, threemf_content_types_location}, "There may only be these two files.")
+			self.assertEqual(archive.read(threemf_rels_location), threemf_rels_xml.encode("UTF-8"), "Correct content for rels file.")
+			self.assertEqual(archive.read(threemf_content_types_location), threemf_content_types_xml.encode("UTF-8"), "Correct content for content types file.")
+		finally:
+			if "file_path" in locals():
+				os.remove(file_path)
